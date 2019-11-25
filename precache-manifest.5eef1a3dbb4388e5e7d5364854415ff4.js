fe06249885065e5a4679ed221a157f187e59e92a
jenia1992 <33311012+jenia1992@users.noreply.github.com>
2019-11-25 03:27:10 +0200

    Updates

---
+++ precache-manifest.5eef1a3dbb4388e5e7d5364854415ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1754d381e4062ac16794ca8c848e6905",
+    "url": "/Yevgeny-Trach-21-11-2019/index.html"
+  },
+  {
+    "revision": "9354c07e5dec919b809e",
+    "url": "/Yevgeny-Trach-21-11-2019/static/css/main.2c8c4cf8.chunk.css"
+  },
+  {
+    "revision": "b641901f065bbfef4972",
+    "url": "/Yevgeny-Trach-21-11-2019/static/js/2.264c464f.chunk.js"
+  },
+  {
+    "revision": "9354c07e5dec919b809e",
+    "url": "/Yevgeny-Trach-21-11-2019/static/js/main.94fbef00.chunk.js"
+  },
+  {
+    "revision": "7798d741032f51f0c8de",
+    "url": "/Yevgeny-Trach-21-11-2019/static/js/runtime-main.5f3c1a95.js"
+  },
+  {
+    "revision": "20e285d9d6e3690736fa1336050ef244",
+    "url": "/Yevgeny-Trach-21-11-2019/static/media/bgClouds.20e285d9.jpg"
+  }
+]);
